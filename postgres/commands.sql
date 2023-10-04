11ad527efa6a4019f5db25b38b67cc9ebb2f1751
Patrick Wang <wang.patrick57@gmail.com>
2023-10-04 12:11:26 -0400

    added csv log symlink

--- postgres/commands.sql
+++ postgres/commands.sql
@@ -5,3 +5,8 @@ INSERT INTO beaker (volume_ml, manufacturer, date_acquired, is_sterile)
 VALUES (1000, 'LabTech', '2023-02-15', FALSE);
 INSERT INTO beaker (volume_ml, manufacturer, date_acquired, is_sterile)
 VALUES (250, 'ScienceStuff', '2021-12-20', TRUE);
+
+-- testing place
+INSERT INTO beaker (volume_ml, manufacturer, date_acquired, is_sterile)
+VALUES ('hi', 55, '2021-12-21', FALSE);
+SELECT * FROM beaker;
