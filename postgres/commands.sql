2c673fcba802f22fed496d6e5e1dcf8060aa8079
Patrick Wang <wang.patrick57@gmail.com>
2023-11-01 16:20:13 -0400

    logging back on

--- postgres/commands.sql
+++ postgres/commands.sql
@@ -5,6 +5,8 @@ VALUES (250, 'LabTech', '2021-12-21', FALSE);
 UPDATE beaker SET manufacturer = 'LabGrow' WHERE volume_ml < 300;
 UPDATE beaker SET manufacturer = 'ShrinkSource' WHERE is_sterile = FALSE;
 
+BEGIN;
 SELECT * FROM beaker;
 SELECT * FROM beaker WHERE volume_ml = 250;
 SELECT * FROM beaker WHERE volume_ml = 300;
+ABORT;
