6e9fa40dae8e33bede2bd65e2ce6474075b5ab86
Patrick Wang <wang.patrick57@gmail.com>
2023-11-05 01:08:49 +0000

    testing speedup

--- d7pg/commands.sql
+++ d7pg/commands.sql
@@ -1,28 +1,18 @@
--- testing place
 INSERT INTO beaker (volume_ml, manufacturer, date_acquired, is_sterile)
 VALUES (250, 'LabTech', '2021-12-21', FALSE);
 
 UPDATE beaker SET manufacturer = 'LabGrow' WHERE volume_ml < 300;
 UPDATE beaker SET manufacturer = 'ShrinkSource' WHERE is_sterile = FALSE;
 
-BEGIN;
-SELECT * FROM beaker;
-SELECT * FROM beaker WHERE volume_ml = 250;
-SELECT * FROM beaker WHERE volume_ml = 400;
-ABORT;
+SELECT pg_sleep(5);
 
 BEGIN;
 SELECT * FROM beaker;
+SELECT pg_sleep(5);
 SELECT * FROM beaker WHERE volume_ml = 250;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
-SELECT * FROM beaker WHERE volume_ml = 400;
+SELECT pg_sleep(5);
 SELECT * FROM beaker WHERE volume_ml = 400;
 ABORT;
 
-UPDATE beaker SET manufacturer = 'LabGrow' WHERE volume_ml < 300;
-UPDATE beaker SET manufacturer = 'ShrinkSource' WHERE is_sterile = FALSE;
+-- random query which starts at late timestamp
+SELECT * FROM beaker;
