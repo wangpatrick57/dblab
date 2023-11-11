83947e26ad9a14b7b372b84c012cb2b11de80759
Patrick Wang <wang.patrick57@gmail.com>
2023-11-11 00:57:36 +0000

    lab no sleep

--- d7pg/commands.sql
+++ d7pg/commands.sql
@@ -4,13 +4,13 @@ VALUES (250, 'LabTech', '2021-12-21', FALSE);
 UPDATE beaker SET manufacturer = 'LabGrow' WHERE volume_ml < 300;
 UPDATE beaker SET manufacturer = 'ShrinkSource' WHERE is_sterile = FALSE;
 
-SELECT pg_sleep(5);
+-- SELECT pg_sleep(5);
 
 BEGIN;
 SELECT * FROM beaker;
-SELECT pg_sleep(5);
+-- SELECT pg_sleep(5);
 SELECT * FROM beaker WHERE volume_ml = 250;
-SELECT pg_sleep(5);
+-- SELECT pg_sleep(5);
 SELECT * FROM beaker WHERE volume_ml = 400;
 ABORT;
 
